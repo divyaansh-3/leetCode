728d4ecd5c3168e90487aad8e97f931cc9975606
divyaansh-3 <parmardivyaansh8@gmail.com>
2026-08-22 13:10:17 +0530

    Time: 4 ms (12.91%), Space: 263.3 MB (11.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -16,7 +16,9 @@ public:
         // reverse the array as per k
         // 5 6 7 1 2 3 4
         if(k%nums.size()==0)return;
+        // if size of array and no of rotations is same then resultnat array will be same.
         k= k% nums.size();
+        // if k=11 and n=7 then after 7 rotations will get same array so resultant array depends on 4.
 
         rev(nums, 0 , nums.size()-1);
         rev(nums, 0 , k-1);
