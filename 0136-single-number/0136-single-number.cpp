60495fc8d5ceca8b3c3a438531b57a3fc1447f4b
divyaansh-3 <parmardivyaansh8@gmail.com>
2026-08-23 21:39:47 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.2%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         // in this case we need to check the frequency so we must use unordered_map 
-        unordered_map<int,int>s;
+       /* unordered_map<int,int>s;
         for(int i=0;i<nums.size();i++){
             s[nums[i]]++;
         }
@@ -10,6 +10,18 @@ public:
             if(s[nums[i]]==1) return nums[i];
         }
         return -1;
+        //but in this case if we use unordered map the space complexity is 0(n)
+        //to reduce the time complexity to constant we need to use xor function
+        // concept is the xor of same numbers is 0.
+        //so the xor of all the numbers having duplicates will be finally 0 and then the xor of 0 and the single digit will be the digit itself
+        */
+
+        int res=0;
+        for(int num:nums){
+            res= res^num;
+        }
+        return res;
+
         
     }
 };
